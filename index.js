b0de2c222c75fb377d3ca158b9303ccef6a7ce66
Huan LI <zixia@zixia.net>
2018-04-30 01:17:49 +0800

    add editorconfig

--- index.js
+++ index.js
@@ -7,41 +7,41 @@
 
 var installed = false
 
-function finis(callback) {
-
-  // attach user callback to the process event emitter.
-  // if no callback, it will still exit gracefully on Ctrl-C
-  if (callback) {
-    process.on('finis', callback)
-  }
-
-  // only install the termination handlers once
-  if (!installed) {
-    install()
-    installed = true
-  }
-}
-
-function install() {
+function install () {
   // do app-specific cleaning before exiting
   process.on('exit',              code  => process.emit('finis', code, 'exit'))
 
   /**
    * catch `ctrl+c` & `kill -s TERM` event
-   * 
+   *
    * https://en.wikipedia.org/wiki/Signal_(IPC)
    * SIGINT	2	Terminate	Terminal interrupt signal.
    * SIGTERM	15	Terminate	Termination signal.
-   * 
+   *
    * https://nodejs.org/api/process.html#process_signal_events
    * SIGTERM and SIGINT have default handlers on non-Windows platforms that resets the terminal mode before exiting with code 128 + signal number. If one of these signals has a listener installed, its default behavior will be removed (Node.js will no longer exit).
    */
-  process.on('SIGINT',            ()  => process.emit('finis', 130, 'SIGINT'))  
+  process.on('SIGINT',            ()  => process.emit('finis', 130, 'SIGINT'))
   process.on('SIGTERM',           ()  => process.emit('finis', 143, 'SIGTERM'))
 
   //catch uncaught exceptions, trace, then exit normally
   process.on('uncaughtException', err => process.emit('finis', 99, 'uncaughtException', err))
 }
+
+function finis (callback) {
+
+    // attach user callback to the process event emitter.
+    // if no callback, it will still exit gracefully on Ctrl-C
+    if (callback) {
+      process.on('finis', callback)
+    }
+
+    // only install the termination handlers once
+    if (!installed) {
+      install()
+      installed = true
+    }
+  }
 
 module.exports = finis.default = finis.finis = finis
 
